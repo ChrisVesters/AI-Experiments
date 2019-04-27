e8724fec8876b4bb16ed180ed26794cfadeb98eb
Chris Vesters <vesters_chris@hotmail.com>
2019-04-27 12:16:49 +0200

    Use function/member pointers for probability.
    - Get rid of obsolete class and use functions instead.
    - Function/member pointers allows to get rid of the indirect method.

--- examples/Object.cpp
+++ examples/Object.cpp
@@ -3,17 +3,21 @@
 Object::Object(const std::string& shape, const std::string& colour) :
 		shape(shape), colour(colour) {
 }
-;
 
-fp Object::matchesShape(const std::string& shape) {
-	return [shape](const Object& obj) {
-		return obj.shape == shape;
-	};
+bool Object::hasShape(const std::string& shape) {
+	return this->shape == shape;
 }
 
-fp Object::matchesColour(const std::string& colour) {
-	return [colour](const Object& obj) {
-		return obj.colour == colour;
-	};
+bool Object::hasColour(const std::string& colour) {
+	return this->colour == colour;
+}
+
+
+std::string Object::* Object::shapeField() {
+	return &Object::shape;
+}
+
+std::string Object::* Object::colourField() {
+	return &Object::colour;
 }
 
---
+++ examples/Probability.cpp
@@ -0,0 +1,46 @@
+#include "Examples.hpp"
+#include "Object.hpp"
+#include "Probability.hpp"
+
+#include <iostream>
+
+void probabilityExample() {
+	std::vector<Object> dataset2;
+	for (int i = 0; i < 100; ++i) {
+		Object o {"round", "red"};
+		dataset2.emplace_back(o);
+
+		Object p {"round", "orange"};
+		dataset2.emplace_back(p);
+
+		Object q {"oval", "green"};
+		dataset2.emplace_back(q);
+
+		Object r {"oval", "red"};
+		dataset2.emplace_back(r);
+
+		Object s {"square", "red"};
+		dataset2.emplace_back(s);
+	}
+
+	std::function<bool(Object)> fSquare = std::bind(&Object::hasShape, std::placeholders::_1, "square");
+	std::function<bool(Object)> fRound = std::bind(&Object::hasShape, std::placeholders::_1, "round");
+	std::function<bool(Object)> fOval = std::bind(&Object::hasShape, std::placeholders::_1, "oval");
+
+	std::cout << maximumLikelihood(fSquare, dataset2) << std::endl;
+	std::cout << maximumLikelihood(fRound, dataset2) << std::endl;
+	std::cout << maximumLikelihood(fOval, dataset2) << std::endl;
+	std::cout << "-------" << std::endl;
+	std::cout << maximumLikelihood<Object, std::string>(Object::shapeField(), "square", dataset2) << std::endl;
+	std::cout << maximumLikelihood<Object, std::string>(Object::shapeField(), "round", dataset2) << std::endl;
+	std::cout << maximumLikelihood<Object, std::string>(Object::shapeField(), "oval", dataset2) << std::endl;
+	std::cout << "-------" << std::endl;
+
+	std::cout << laplaceSmoothing(fSquare, 3, 1, dataset2) << std::endl;
+	std::cout << laplaceSmoothing(fRound, 3, 1, dataset2) << std::endl;
+	std::cout << laplaceSmoothing(fOval, 3, 1, dataset2) << std::endl;
+	std::cout << "-------" << std::endl;
+	std::cout << laplaceSmoothing<Object, std::string>(Object::shapeField(), "square", 1, dataset2) << std::endl;
+	std::cout << laplaceSmoothing<Object, std::string>(Object::shapeField(), "round", 1, dataset2) << std::endl;
+	std::cout << laplaceSmoothing<Object, std::string>(Object::shapeField(), "oval", 1, dataset2) << std::endl;
+}
